6f4fc65c1294aa32046fe0c2820df545dcdb7c34
Andrey Linnik <av.linnik79@ya.ru>
2023-05-22 17:48:30 +0300

    Добавьте файлы проекта.

---
+++ Part2Linq/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var classes = new[]
+        {
+              new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+        var allStudents = GetAllStudents(classes);
+
+        Console.WriteLine(string.Join(" ", allStudents));
+    }
+
+    static string[] GetAllStudents(Classroom[] classes)
+    {
+
+        var allStudents = classes.SelectMany(s => s.Students).ToArray();
+
+        return allStudents;
+
+    }
+
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+}
+}
